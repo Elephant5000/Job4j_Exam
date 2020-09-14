c5c83d9eb96bfbcca94fe910f75996e167fc9b23
Elephant5000 <pashau@mail.ru>
2020-09-14 17:29:46 +0300

    1.2.3. Создание мультиактивности. Передача данных между активностями. [#351688] динамическое формирование протокола

--- app/src/main/java/ru/job4j/exam/ResultActivity.java
+++ app/src/main/java/ru/job4j/exam/ResultActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
@@ -15,16 +16,27 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result_activity);
+
+        LinearLayout linearLayoutVertical = findViewById(R.id.linearLayoutVertical);
+
         for (int index = 0; index < store.size(); index ++) {
-            TextView question = findViewById(getResources().getIdentifier("questionTV" + String.valueOf(index), "id", this.getPackageName()));
-            TextView answer = findViewById(getResources().getIdentifier("answerTV" + String.valueOf(index), "id", this.getPackageName()));
-            TextView userAnswer = findViewById(getResources().getIdentifier("userAnswerTV" + String.valueOf(index), "id", this.getPackageName()));
-            question.setText(store.get(index).getText());
-            answer.setText(String.valueOf(store.get(index).getAnswer()));
-            userAnswer.setText(String.valueOf(store.get(index).getUserAnswer()));
+            View view = getLayoutInflater().inflate(R.layout.result_question_item, null);
+
+            TextView questionN = (TextView) view.findViewById(R.id.questionTV);
+            TextView answerN = view.findViewById(R.id.answerTV);
+            TextView userAnswerN = view.findViewById(R.id.userAnswerTV);
+
+            questionN.setText(store.get(index).getText());
+            answerN.setText(String.valueOf(store.get(index).getAnswer()));
+            userAnswerN.setText(String.valueOf(store.get(index).getUserAnswer()));
+
+            linearLayoutVertical.addView(view);
         }
-        Button back = findViewById(R.id.back);
-        back.setOnClickListener(this::backBtn);
+
+        View view = getLayoutInflater().inflate(R.layout.back_button, null);
+        Button button = view.findViewById(R.id.back);
+        linearLayoutVertical.addView(view);
+        button.setOnClickListener(this::backBtn);
     }
 
     private void backBtn(View view) {
