a244434c8a6649ccb518d0135e2b5d6918d26f52
Elephant5000 <pashau@mail.ru>
2020-09-17 15:04:44 +0300

    1.2.4.1. Пакетные аргументы. [#351692]

--- app/src/main/java/ru/job4j/exam/hintactivity/HintActivity.java
+++ app/src/main/java/ru/job4j/exam/hintactivity/HintActivity.java
@@ -2,13 +2,16 @@ package ru.job4j.exam.hintactivity;
 
 import androidx.fragment.app.Fragment;
 import ru.job4j.exam.BaseActivity;
+import ru.job4j.exam.examactivity.ExamActivity;
 
 
 public class HintActivity extends BaseActivity {
 
     @Override
     public Fragment loadFrg() {
-        return new HintFragment();
+        return HintFragment.of(
+                getIntent().getIntExtra(ExamActivity.HINT_FOR, 0)
+        );
     }
 
 }
--- app/src/main/java/ru/job4j/exam/hintactivity/HintFragment.java
+++ app/src/main/java/ru/job4j/exam/hintactivity/HintFragment.java
@@ -20,28 +20,39 @@ import ru.job4j.exam.examactivity.ExamActivity;
 
 public class HintFragment extends Fragment {
 
-    private View viewHintFragment = null;
     private final Map<Integer, String> answer = new HashMap<Integer, String>();
-
     private final QuestionStore store = QuestionStore.getInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        viewHintFragment = inflater.inflate(R.layout.hint_activity, container, false);
-
-        for (int index = 0; index < store.size(); index ++) {
-            this.answer.put(index, "Answer -  " + store.get(index).getAnswer());
-        }
+        View viewHintFragment = inflater.inflate(R.layout.hint_activity, container, false);
 
         TextView hintTextView = viewHintFragment.findViewById(R.id.hint);
         TextView questionTextView = viewHintFragment.findViewById(R.id.question);
-        int question = Objects.requireNonNull(getActivity()).getIntent().getIntExtra(ExamActivity.HINT_FOR, 0);
+
+        //int question = Objects.requireNonNull(getActivity()).getIntent().getIntExtra(ExamActivity.HINT_FOR, 0);
+        int question = getArguments().getInt(ExamActivity.HINT_FOR, 0);
+
         hintTextView.setText(this.answer.get(question));
         questionTextView.setText(store.get(question).getText());
         Button back = viewHintFragment.findViewById(R.id.back);
         back.setOnClickListener(this::backBtn);
         return viewHintFragment;
+    }
+
+    public HintFragment() {
+        for (int index = 0; index < store.size(); index ++) {
+            this.answer.put(index, "Answer -  " + store.get(index).getAnswer());
+        }
+    }
+
+    public static HintFragment of(int index) {
+        HintFragment hint = new HintFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ExamActivity.HINT_FOR, index);
+        hint.setArguments(bundle);
+        return hint;
     }
 
     private void backBtn(View view) {
