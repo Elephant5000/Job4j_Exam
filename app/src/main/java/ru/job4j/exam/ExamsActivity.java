c303aa73bc497322347cf974ac229ce8925db980
Elephant5000 <pashau@mail.ru>
2020-11-03 20:51:38 +0300

    1.3.2. Виджет - список. [#351696] Заготовка 2

--- app/src/main/java/ru/job4j/exam/ExamsActivity.java
+++ app/src/main/java/ru/job4j/exam/ExamsActivity.java
@@ -1,6 +1,7 @@
 package ru.job4j.exam;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,11 +15,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.security.AccessController;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.security.AccessController.*;
+import ru.job4j.exam.examactivity.ExamActivity;
+
 
 public class ExamsActivity extends AppCompatActivity {
     private RecyclerView recycler;
@@ -35,62 +38,70 @@ public class ExamsActivity extends AppCompatActivity {
     private void updateUI() {
         List<Exam> exams = new ArrayList<Exam>();
         for (int index = 0; index != 10; index++) {
-            exams.add(new Exam(index, String.format("Exam %s", index), System.currentTimeMillis(), index));
+            exams.add(new Exam(index, String.format("Exam %s", index), System.currentTimeMillis(), 0));
         }
         this.recycler.setAdapter(new ExamAdapter(exams));
     }
 
-}
 
 
-class ExamAdapter extends RecyclerView.Adapter<ExamHolder> {
 
-    private  final List<Exam> exams;
+    public class ExamAdapter extends RecyclerView.Adapter<ExamHolder> {
 
-    public ExamAdapter(List<Exam> exams) {
-        this.exams = exams;
-    }
+        private  final List<Exam> exams;
 
-    @NonNull
-    @Override
-    public ExamHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View view = inflater.inflate(R.layout.info_exam, parent, false);
-        return new ExamHolder(view);
-    }
+        public ExamAdapter(List<Exam> exams) {
+            this.exams = exams;
+        }
 
-    @Override
-    public void onBindViewHolder(@NonNull ExamHolder holder, int position) {
-        final Exam exam = this.exams.get(position);
-        TextView text = holder.view.findViewById(R.id.info);
-        text.setText(exam.getName());
 
-        text.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
+        @NonNull
+        @Override
+        public ExamHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            View view = inflater.inflate(R.layout.info_exam, parent, false);
+            return new ExamHolder(view);
+        }
 
-                        Toast.makeText(holder.view.getContext(), "You select " + exam, Toast.LENGTH_SHORT).show();
-                    }
+        @Override
+        public void onBindViewHolder(@NonNull ExamHolder holder, int position) {
+            final Exam exam = this.exams.get(position);
+            TextView examName = holder.view.findViewById(R.id.info);
+            TextView examResult = holder.view.findViewById(R.id.result);
+            TextView examDate = holder.view.findViewById(R.id.date);
+            DateFormat dateFormat = new SimpleDateFormat("HH:mm dd:MM:yyyy");
+            examName.setText(exam.getName());
+            examResult.setText(String.valueOf(exam.getResult()));
+            examDate.setText((exam.getResult()  != 0) ? dateFormat.format(exam.getTime()) : "Empty");
+
+            examName.setOnClickListener(
+                v -> {
+                    //Toast.makeText(holder.view.getContext(), "You select " + exam, Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(ExamsActivity.this, ExamActivity.class);
+                    //intent.putExtra(ExamActivity.HINT_FOR, store.getPosition());
+                    startActivity(intent);
                 }
-        );
+            );
+        }
 
+        @Override
+        public int getItemCount() {
+            return this.exams.size();
+        }
     }
 
-    @Override
-    public int getItemCount() {
-        return this.exams.size();
-    }
-}
 
 
-class ExamHolder extends RecyclerView.ViewHolder {
 
-    public View view;
+    public static class ExamHolder extends RecyclerView.ViewHolder {
 
-    public ExamHolder(@NonNull View view) {
-        super(view);
-        this.view = itemView;
+        public View view;
+
+        public ExamHolder(@NonNull View view) {
+            super(view);
+            this.view = itemView;
+        }
+
     }
 
 }
